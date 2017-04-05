1e079098ba735b543ee7576addc3607de654e8a2
Renaldas Zioma <rej@unity3d.com>
2017-04-05 20:06:29 +0200

    Strip unused code

--- neural_gen.py
+++ neural_gen.py
@@ -13,16 +13,12 @@ Script Usage:
     --target-image-prefix generated/wall
     ```
 
-References:
+Base on:
 [Dmitry Ulyanov's blog on fast-neural-doodle](http://dmitryulyanov.github.io/feed-forward-neural-doodle/)
 [Torch code for fast-neural-doodle](https://github.com/DmitryUlyanov/fast-neural-doodle)
 [Torch code for online-neural-doodle](https://github.com/DmitryUlyanov/online-neural-doodle)
 [Paper Texture Networks: Feed-forward Synthesis of Textures and Stylized Images](http://arxiv.org/abs/1603.03417)
 [Discussion on parameter tuning](https://github.com/fchollet/keras/issues/3705)
-
-Resources:
-Example images can be downloaded from
-https://github.com/DmitryUlyanov/fast-neural-doodle/tree/master/data
 '''
 from __future__ import print_function
 import time
@@ -52,12 +48,6 @@ add_arg('--variation',              default=50, type=float,         help='todo')
 args = parser.parse_args()
 
 num_iterations = args.iterations
-###style_mask_path = args.style_mask
-###target_mask_path = args.target_mask
-###content_img_path = args.content_image
-###use_content_img = content_img_path is not None
-
-###num_labels = args.nlabels
 num_colors = 3  # RGB
 # determine image sizes based on style_image
 target_size = imread(args.style_image).shape[:2]
@@ -66,9 +56,6 @@ if args.target_size:
 img_nrows, img_ncols = target_size
 
 total_variation_weight = args.variation
-###style_weight = 1.
-###content_weight = 0.1 if use_content_img else 0
-###content_feature_layers = ['block5_conv2']
 
 # To get better generation qualities, use more conv layers for style features
 style_feature_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
@@ -76,7 +63,6 @@ style_feature_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_
 
 # index constants for images and tasks variables
 STYLE, TARGET, CONTENT = 0, 1, 2
-
 
 # helper functions for reading/processing images
 def preprocess_image(image_path, target_size):
@@ -122,57 +108,6 @@ def deprocess_image_partially(x):
     x = np.clip(x, 0, 255).astype('uint8')
     return x
 
-def rescale_image(self, img, scale):
-    """Re-implementing skimage.transform.scale without the extra dependency. Saves a lot of space and hassle!
-    """
-    output = scipy.misc.toimage(img, cmin=0.0, cmax=255)
-    output.thumbnail((int(output.size[0]*scale), int(output.size[1]*scale)), PIL.Image.ANTIALIAS)
-    return np.asarray(output)
-"""
-def kmeans(xs, k):
-    assert xs.ndim == 2
-    try:
-        from sklearn.cluster import k_means
-        _, labels, _ = k_means(xs.astype('float64'), k)
-    except ImportError:
-        from scipy.cluster.vq import kmeans2
-        _, labels = kmeans2(xs, k, missing='raise')
-    return labels
-
-
-def load_mask_labels():
-    '''Load both target and style masks.
-    A mask image (nr x nc) with m labels/colors will be loaded
-    as a 4D boolean tensor: (1, m, nr, nc) for 'channels_first' or (1, nr, nc, m) for 'channels_last'
-    '''
-    target_mask_img = load_img(target_mask_path,
-                               target_size=(img_nrows, img_ncols))
-    target_mask_img = img_to_array(target_mask_img)
-    style_mask_img = load_img(style_mask_path,
-                              target_size=(img_nrows, img_ncols))
-    style_mask_img = img_to_array(style_mask_img)
-    if K.image_data_format() == 'channels_first':
-        mask_vecs = np.vstack([style_mask_img.reshape((3, -1)).T,
-                               target_mask_img.reshape((3, -1)).T])
-    else:
-        mask_vecs = np.vstack([style_mask_img.reshape((-1, 3)),
-                               target_mask_img.reshape((-1, 3))])
-
-    labels = kmeans(mask_vecs, num_labels)
-    style_mask_label = labels[:img_nrows *
-                              img_ncols].reshape((img_nrows, img_ncols))
-    target_mask_label = labels[img_nrows *
-                               img_ncols:].reshape((img_nrows, img_ncols))
-
-    stack_axis = 0 if K.image_data_format() == 'channels_first' else -1
-    style_mask = np.stack([style_mask_label == r for r in xrange(num_labels)],
-                          axis=stack_axis)
-    target_mask = np.stack([target_mask_label == r for r in xrange(num_labels)],
-                           axis=stack_axis)
-
-    return (np.expand_dims(style_mask, axis=0),
-            np.expand_dims(target_mask, axis=0))
-"""
 
 # Define loss functions
 def gram_matrix(x):
@@ -180,28 +115,6 @@ def gram_matrix(x):
     features = K.batch_flatten(x)
     gram = K.dot(features, K.transpose(features))
     return gram
-
-"""
-def region_style_loss(style_image, target_image, style_mask, target_mask):
-    '''Calculate style loss between style_image and target_image,
-    for one common region specified by their (boolean) masks
-    '''
-    assert 3 == K.ndim(style_image) == K.ndim(target_image)
-    assert 2 == K.ndim(style_mask) == K.ndim(target_mask)
-    if K.image_data_format() == 'channels_first':
-        masked_style = style_image * style_mask
-        masked_target = target_image * target_mask
-        num_channels = K.shape(style_image)[0]
-    else:
-        masked_style = K.permute_dimensions(
-            style_image, (2, 0, 1)) * style_mask
-        masked_target = K.permute_dimensions(
-            target_image, (2, 0, 1)) * target_mask
-        num_channels = K.shape(style_image)[-1]
-    s = gram_matrix(masked_style) / K.mean(style_mask) / num_channels
-    c = gram_matrix(masked_target) / K.mean(target_mask) / num_channels
-    return K.mean(K.square(s - c))
-"""
 
 def region_style_loss(style_image, target_image):
     '''Calculate style loss between style_image and target_image,
@@ -222,27 +135,6 @@ def region_style_loss(style_image, target_image):
     c = gram_matrix(target) / K.cast(num_channels, K.floatx())
     return K.mean(K.square(s - c))
 
-"""
-def style_loss(style_image, target_image, style_masks, target_masks):
-    '''Calculate style loss between style_image and target_image,
-    in all regions.
-    '''
-    assert 3 == K.ndim(style_image) == K.ndim(target_image)
-    assert 3 == K.ndim(style_masks) == K.ndim(target_masks)
-    loss = K.variable(0)
-    for i in xrange(num_labels):
-        if K.image_data_format() == 'channels_first':
-            style_mask = style_masks[i, :, :]
-            target_mask = target_masks[i, :, :]
-        else:
-            style_mask = style_masks[:, :, i]
-            target_mask = target_masks[:, :, i]
-        loss += region_style_loss(style_image,
-                                  target_image, style_mask, target_mask)
-
-    return loss
-"""
-
 def style_loss(style_image, target_image):
     '''Calculate style loss between style_image and target_image,
     in all regions.
@@ -253,11 +145,6 @@ def style_loss(style_image, target_image):
                               target_image)
 
     return loss
-
-"""
-def content_loss(content_image, target_image):
-    return K.sum(K.square(target_image - content_image))
-"""
 
 def total_variation_loss(x):
     assert 4 == K.ndim(x)
@@ -313,19 +200,10 @@ class Evaluator(object):
         # Overall loss is the weighted sum of content_loss, style_loss and tv_loss
         # Each individual loss uses features from image/mask models.
         loss = K.variable(0)
-        """for layer in content_feature_layers:
-            content_feat = image_features[layer][CONTENT, :, :, :]
-            target_feat = image_features[layer][TARGET, :, :, :]
-            loss += content_weight * content_loss(content_feat, target_feat)
-        """
         for layer in style_feature_layers:
             style_feat = image_features[layer][STYLE, :, :, :]
             target_feat = image_features[layer][TARGET, :, :, :]
-            ###style_masks = mask_features[layer][STYLE, :, :, :]
-            ###target_masks = mask_features[layer][TARGET, :, :, :]
-            ###sl = style_loss(style_feat, target_feat, style_masks, target_masks)
             sl = style_loss(style_feat, target_feat)
-            ###loss += (style_weight / len(style_feature_layers)) * sl
             loss += len(style_feature_layers) * sl
 
         loss += total_variation_weight * total_variation_loss(target_image)
@@ -353,7 +231,7 @@ class Evaluator(object):
             grad_values = np.array(outs[1:]).flatten().astype('float64')
         return loss_value, grad_values
 
-###                     
+    ###                     
 
     def loss(self, x):
         assert self.loss_value is None
@@ -368,8 +246,6 @@ class Evaluator(object):
         self.loss_value = None
         self.grad_values = None
         return grad_values
-
-###evaluator = Evaluator()
 
 iter = int(0)
 t0 = time.time()
@@ -414,15 +290,6 @@ for q in range(args.phases):
         fname = args.target_image_prefix + '_after_upscale_%d.png' % q
         imsave(fname, img)
         print('Image saved as', fname)
-
-        """
-        x = imresize(x.copy()-128, (img_nrows, img_ncols), interp='bicubic')
-        #x = x.transpose((2, 0, 1))[np.newaxis]
-        img = deprocess_image(img_to_array(x.copy()))
-        fname = args.target_image_prefix + '_after_upscale_%d.png' % q
-        imsave(fname, img)
-        print('Image saved as', fname)
-        """
 
     # Optimization algorithm needs min and max bounds to prevent divergence.
     #data_bounds = np.zeros((np.product(x.shape), 2), dtype=np.float64)                             # ??? float64
